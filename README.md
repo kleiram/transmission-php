118518d45b3a517d4689e6b620c0bf2512204b37
Ramon Kleiss <ramonkleiss@gmail.com>
2013-03-28 20:46:34 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PHP Transmission API
---
+++ tests/bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+if (!$loader = @include __DIR__.'/../vendor/autoload.php') {
+    echo <<<EOM
+    Install dependencies using Composer:
+
+        curl -s https://getcomposer.org/installer | php
+        php composer.phar install
+
+EOM;
+
+    exit(1);
+}
+
+$loader->add('Transmission\Test', __DIR__);
