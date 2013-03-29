720295985979530a5146a27864320c5dde72f688
Ramon Kleiss <ramonkleiss@gmail.com>
2013-03-29 13:33:36 +0100

    Created the Torrent model

---
+++ lib/Transmission/Model/Torrent.php
@@ -0,0 +1,161 @@
+<?php
+namespace Transmission\Model;
+
+/**
+ * @author Ramon Kleiss <ramon@cubilon.nl>
+ */
+class Torrent
+{
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var string
+     */
+    protected $name;
+
+    /**
+     * @var integer
+     */
+    protected $size;
+
+    /**
+     * @var DateTime
+     */
+    protected $doneDate
+
+    /**
+     * @var array
+     */
+    protected $files;
+
+    /**
+     * @var array
+     */
+    protected $peers;
+
+    /**
+     * @var array
+     */
+    protected $trackers;
+
+    /**
+     * @param integer $id
+     */
+    public function setId($id)
+    {
+        $this->id = (integer) $id;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId($id)
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = (string) $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param integer $size
+     */
+    public function setSize($size)
+    {
+        $this->size = (integer) $size;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getSize()
+    {
+        return $this->size;
+    }
+
+    /**
+     * @param integer|DateTime $doneDate
+     */
+    public function setDoneDate($doneDate)
+    {
+        if ($doneDate instanceof \DateTime) {
+            $this->doneDate = $doneDate;
+
+            return;
+        }
+
+        $this->doneDate = new \DateTime((integer) $doneDate);
+    }
+
+    /**
+     * @return DateTime
+     */
+    public function getDoneDate()
+    {
+        return $this->doneDate;
+    }
+
+    /**
+     * @param Transmission\Model\File $file
+     */
+    public function addFile(File $file)
+    {
+        $this->files[] = $file;
+    }
+
+    /**
+     * @return array
+     */
+    public function getFiles()
+    {
+        return $this->files;
+    }
+
+    /**
+     * @param Transmission\Model\Peer $peer
+     */
+    public function addPeer(Peer $peer)
+    {
+        $this->peers[] = $peer;
+    }
+
+    /**
+     * @return array
+     */
+    public function getPeers()
+    {
+        return $this->peers;
+    }
+
+    /**
+     * @param Transmission\Model\Tracker $tracker
+     */
+    public function addTracker(Tracker $tracker)
+    {
+        $this->trackers[] = $tracker;
+    }
+
+    /**
+     * @return array
+     */
+    public function getTrackers()
+    {
+        return $this->tracker;
+    }
+}
