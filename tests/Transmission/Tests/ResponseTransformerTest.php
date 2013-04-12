4b47a33b2564a1429b4fea367fcc427df6799bfc
Ramon Kleiss <ramonkleiss@gmail.com>
2013-04-12 18:16:49 +0200

    Added tests for the ResponseTransformer

--- tests/Transmission/Tests/ResponseTransformerTest.php
+++ tests/Transmission/Tests/ResponseTransformerTest.php
@@ -33,11 +33,49 @@ class ResponseTransformerTest extends \PHPUnit_Framework_TestCase
         $result = ResponseTransformer::transform(
             $response,
             $subject,
-            array('id', 'foo' => 'bar', 'lorem' => 'ipsum')
+            array('id', 'foo' => 'bar')
         );
 
         $this->assertInstanceOf('Transmission\Tests\Foo', $result);
         $this->assertEquals(1, $subject->id);
         $this->assertEquals('baz', $subject->getBar());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldIgnoreNonExistingFields()
+    {
+        $subject = new Foo();
+        $response = (object) array('id' => 1, 'foo' => 'baz');
+
+        $result = ResponseTransformer::transform(
+            $response,
+            $subject,
+            array('id', 'foo' => 'bar', 'bar' => 'baz')
+        );
+
+        $this->assertInstanceOf('Transmission\Tests\Foo', $result);
+        $this->assertEquals(1, $subject->id);
+        $this->assertEquals('baz', $subject->getBar());
+    }
+
+    /**
+     * @test
+     */
+    public function shouldIgnoreFieldsWhereNullIsSpecified()
+    {
+        $subject = new Foo();
+        $response = (object) array('id' => 1, 'foo' => 'baz');
+
+        $result = ResponseTransformer::transform(
+            $response,
+            $subject,
+            array('id', 'foo' => null)
+        );
+
+        $this->assertInstanceOf('Transmission\Tests\Foo', $result);
+        $this->assertEquals(1, $subject->id);
+        $this->assertNull($subject->getBar());
     }
 }
