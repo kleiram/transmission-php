4da1e63fc2f6a90cc0ee2c0af369aa9cfa05eda3
Ramon Kleiss <ramonkleiss@gmail.com>
2013-04-13 02:27:24 +0200

    Fixed the queue example

--- examples/queue.php
+++ examples/queue.php
@@ -1,45 +1,11 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
-function formatBytes($bytes, $precision = 2) {
-    $units = array('B', 'KB', 'MB', 'GB', 'TB');
-
-    $bytes = max($bytes, 0);
-    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
-    $pow = min($pow, count($units) - 1);
-
-    // Uncomment one of the following alternatives
-    $bytes /= pow(1024, $pow);
-    // $bytes /= (1 << (10 * $pow));
-
-    return round($bytes, $precision) . ' ' . $units[$pow];
-}
-
 // Find all the torrents
 $queue = Transmission\Torrent::all();
 
 echo sprintf("Found %d torrents\n", count($queue));
 
-$totalSize = 0;
-
 foreach ($queue as $torrent) {
-    $size = formatBytes($torrent->getSize(), 2);
     echo "#{$torrent->getId()}: {$torrent->getName()} ({$size})\n";
-
-    $totalSize += $torrent->getSize();
-}
-
-$totalSize = formatBytes($totalSize);
-echo "\nTotal queue size: {$totalSize}\n";
-
-foreach ($queue as $torrent) {
-    $count = count($torrent->getFiles());
-
-    echo "{$count} files in {$torrent->getName()}:\n";
-
-    foreach ($torrent->getFiles() as $file) {
-        echo "- {$file->getName()}\n";
-    }
-
-    echo "\n";
 }
