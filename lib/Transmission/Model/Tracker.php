3c2bfeca431bdb32330358cbc4c856e933f229bb
Ramon Kleiss <ramonkleiss@gmail.com>
2013-04-27 02:19:15 +0200

    Created the Tracker model

--- lib/Transmission/Model/Tracker.php
+++ lib/Transmission/Model/Tracker.php
@@ -6,4 +6,100 @@ namespace Transmission\Model;
  */
 class Tracker extends AbstractModel
 {
+    /**
+     * @var integer
+     */
+    protected $id;
+
+    /**
+     * @var integer
+     */
+    protected $tier;
+
+    /**
+     * @var string
+     */
+    protected $scrape;
+
+    /**
+     * @var string
+     */
+    protected $announce;
+
+    /**
+     * @param integer $id
+     */
+    public function setId($id)
+    {
+        $this->id = (integer) $id;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param integer $tier
+     */
+    public function setTier($tier)
+    {
+        $this->tier = (integer) $tier;
+    }
+
+    /**
+     * @return integer
+     */
+    public function getTier()
+    {
+        return $this->tier;
+    }
+
+    /**
+     * @param string $scrape
+     */
+    public function setScrape($scrape)
+    {
+        $this->scrape = (string) $scrape;
+    }
+
+    /**
+     * @return string
+     */
+    public function getScrape()
+    {
+        return $this->scrape;
+    }
+
+    /**
+     * @param string $announce
+     */
+    public function setAnnounce($announce)
+    {
+        $this->announce = (string) $announce;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAnnounce()
+    {
+        return $this->announce;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getMapping()
+    {
+        return array(
+            'id' => 'id',
+            'tier' => 'tier',
+            'scrape' => 'scrape',
+            'announce' => 'announce'
+        );
+    }
 }
