ebea1d76bb3ff6ce40c35f565a86b5b25ff96fc7
Ramon Kleiss <ramonkleiss@gmail.com>
2013-04-28 16:07:17 +0200

    Added the queue example

---
+++ examples/queue.php
@@ -0,0 +1,20 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$transmission = new Transmission\Transmission();
+$queue = $transmission->all();
+
+foreach ($queue as $torrent) {
+    echo "{$torrent->getName()}";
+
+    if ($torrent->isFinished()) {
+        echo ": done\n";
+    } else {
+        if ($torrent->isDownloading()) {
+            echo ": {$torrent->getPercentDone()}% ";
+            echo "(eta: ". gmdate("H:i:s", $torrent->getEta()) .")\n";
+        } else{
+            echo ": paused\n";
+        }
+    }
+}
