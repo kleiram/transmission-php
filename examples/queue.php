7ef88d7d443034eea232936d5f59de27883b75a1
Dickriven Chellemboyee <jchellem@gmail.com>
2014-05-11 23:15:55 +0400

    revert queue.php

--- examples/queue.php
+++ examples/queue.php
@@ -2,22 +2,21 @@
 require_once __DIR__.'/../vendor/autoload.php';
 
 $transmission = new Transmission\Transmission();
-$queue = $transmission->getSessionStats();
-print_r($queue);
+$queue = $transmission->all();
 
-// echo "Downloading to: {$transmission->getSession()->getDownloadDir()}\n";
-//
-// foreach ($queue as $torrent) {
-//     echo "{$torrent->getName()}";
-//
-//     if ($torrent->isFinished()) {
-//         echo ": done\n";
-//     } else {
-//         if ($torrent->isDownloading()) {
-//             echo ": {$torrent->getPercentDone()}% ";
-//             echo "(eta: ". gmdate("H:i:s", $torrent->getEta()) .")\n";
-//         } else{
-//             echo ": paused\n";
-//         }
-//     }
-// }
+echo "Downloading to: {$transmission->getSession()->getDownloadDir()}\n";
+
+foreach ($queue as $torrent) {
+    echo "{$torrent->getName()}";
+
+    if ($torrent->isFinished()) {
+        echo ": done\n";
+    } else {
+        if ($torrent->isDownloading()) {
+            echo ": {$torrent->getPercentDone()}% ";
+            echo "(eta: ". gmdate("H:i:s", $torrent->getEta()) .")\n";
+        } else{
+            echo ": paused\n";
+        }
+    }
+}
